700f22372b0e903572835dfa94fede6617a9de74
Gayathri Lakshmi <GayathriLakshmi@wipro.com>
2025-06-12 11:15:51 +0530

    IMS Project

---
+++ Inventory Management System/Inventory Management System/Inventory Management System/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace Inventory_Management_System.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Supplier { get; set; }
+    }
+}
